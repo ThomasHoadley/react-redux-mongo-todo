0e7767c42dadfede40b52aceaf65de7710c59075
Tom Hoadley <hoadleytom@gmail.com>
2020-08-14 16:32:00 +0100

    Ive set it up so that the ListForm props are updated when there is a form submission. I now assume that I need to update the state of Lists with the new list Title and am creating a new branch to test

---
+++ client/src/components/ListForm.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Formik, Field } from 'formik';
+
+
+class ListsForm extends Component {
+	constructor(props) {
+		super(props)
+		this.onFormSubmit = this.props.onFormSubmit;
+	}
+
+	render() {
+		return (
+			<div className="listsForm">
+				<Formik
+					initialValues={{ title: '' }}
+
+					onSubmit={(values, { setSubmitting, resetForm }) => {
+						let title = JSON.stringify(values.title, null, 2);
+						this.onFormSubmit(title);
+						setSubmitting(false);
+						resetForm();
+					}}
+				>
+					{({
+						values,
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						isSubmitting,
+					}) => (
+							<form onSubmit={handleSubmit}>
+								<Field
+									type="text"
+									name="title"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.title}
+									placeholder="List title"
+								/>
+								<button type="submit" disabled={isSubmitting}>
+									Add list
+           		</button>
+							</form>
+						)}
+				</Formik>
+			</div>
+		);
+	}
+}
+
+export default ListsForm;
--- client/src/components/Lists.jsx
+++ client/src/components/Lists.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import ListForm from "./ListForm";
 
 class Lists extends Component {
 	constructor(props) {
 		super(props)
+		this.handleFormSubmit = this.handleFormSubmit.bind(this)
 	}
+
+	handleFormSubmit(e) {
+		console.log(e)
+	}
+
 	render() {
 		return (
 			<div className="lists">
-				<h1>Lists</h1>
 
+				<ListForm onFormSubmit={this.handleFormSubmit} />
+
+				<h1>Lists</h1>
 				{Object.keys(this.props.lists).map((key) => {
 					let listItem = this.props.lists[key]
 					return (
