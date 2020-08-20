89bb597d51c25e2194b5621f52d474cc11955f66
Tom Hoadley <hoadleytom@gmail.com>
2020-08-20 15:34:37 +0100

    Pushing

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Tasks from "./components/Tasks";
 // TODO: Pull in the list from the server file... 
 // TODO: Can this be abstracted? 
 // TODO: Add redirect on add list to new list.
+// TODO: introduce redux / redux toolkit.
 
 class App extends Component {
 	constructor() {
